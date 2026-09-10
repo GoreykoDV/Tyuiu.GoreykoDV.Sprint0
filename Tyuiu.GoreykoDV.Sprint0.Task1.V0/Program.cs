8143d8604f63f129d160c215c56f1f149bba9197
mAssonwilliams <darya.gorat@mail.ru>
2026-09-10 16:10:43 +0500

    t1

--- Tyuiu.GoreykoDV.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.GoreykoDV.Sprint0.Task1.V0/Program.cs
@@ -14,6 +14,7 @@ namespace Tyuiu.GoreykoDV.Sprint0.Task1.V0
         {
             Console.WriteLine("Hello, World");
             string name = "Dasha";
+
             int age = 20;
             Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
             Console.ReadKey();
