ca5e5d11c7d21b77ce9fac41103d7d76b95a65c1
mAssonwilliams <darya.gorat@mail.ru>
2026-09-14 18:20:28 +0500

    Task2

---
+++ Tyuiu.GoreykoDV.Sprint0.Task2.V1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.GoreykoDV.Sprint0.Task2.V1.Lib;
+
+namespace Tyuiu.GoreykoDV.Sprint0.Task2.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Dasha"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ using Tyuiu.GoreykoDV.Sprint0.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GoreykoDV.Sprint0.Task2.V1.Lib;
+
+namespace using_Tyuiu.GoreykoDV.Sprint0.Task2.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Dasha";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Dasha", res);
+        }
+    }
+}
